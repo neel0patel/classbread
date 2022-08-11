8400220434239d414c2e82adb4a874c7e9aa2580
Neel Patel <neelpatel@Neels-MacBook-Pro.local>
2022-08-11 18:47:09 -0400

    sending index and show data

--- breads/server.js
+++ breads/server.js
@@ -6,6 +6,7 @@ const PORT = process.env.PORT;
 const app = express();
 
 // MIDDLEWARE
+app.use(express.static("public"));
 app.set("views", __dirname + "/views");
 app.set("view engine", "jsx");
 app.engine("jsx", require("express-react-views").createEngine());
@@ -18,6 +19,11 @@ app.get("/", (req, res) => {
 // Breads
 const breadsController = require("./controllers/breads_controller.js");
 app.use("/breads", breadsController);
+
+// 404 Page
+app.get("*", (req, res) => {
+  res.send("404");
+});
 
 app.listen(process.env.PORT, function () {
   console.log("IM ALIVE!!");
